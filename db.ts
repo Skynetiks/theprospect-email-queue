a2f0a00ee16a7606d079ab677d1b74022efb5fff
amang06 <aman@skynetiks.com>
2024-06-12 20:44:49 +0530

    connectionstring

--- db.ts
+++ db.ts
@@ -3,14 +3,30 @@ import dotenv from "dotenv";
 
 dotenv.config(); // Load environment variables from .env file
 
+// Check if all required environment variables are present
+// const requiredEnvVariables = [
+//   "DATABASE_USER",
+//   "DATABASE_PASSWORD",
+//   "DATABASE_NAME",
+//   "DATABASE_PORT",
+//   "DATABASE_HOST"
+// ];
+
+// for (const envVar of requiredEnvVariables) {
+//   if (!process.env[envVar]) {
+//     throw new Error(`Environment variable ${envVar} is not defined.`);
+//   }
+// }
+
 const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  port: parseInt(process.env.DATABASE_PORT!),
-  host: process.env.DATABASE_HOST,
-  ssl: true,
-  idleTimeoutMillis: 1
+  connectionString: process.env.DATABASE_URL,
+  // user: process.env.DATABASE_USER,
+  // password: process.env.DATABASE_PASSWORD,
+  // database: process.env.DATABASE_NAME,
+  // port: parseInt(process.env.DATABASE_PORT!),
+  // host: process.env.DATABASE_HOST,
+  // ssl: true,
+  // idleTimeoutMillis: 1000 // Adjust the timeout as needed
 });
 
 // Function to handle queries
